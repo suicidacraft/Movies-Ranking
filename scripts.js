55e44b19f9b90c803fe821ec1339c609336f5b51
Mike Chen <mchen175@go.pasadena.edu>
2026-04-21 16:11:50 -0700

    new

--- scripts.js
+++ scripts.js
@@ -7,27 +7,37 @@
 //Data comes from movie.js It is one big array of objects.
 
 
+let counter = 0;
+
 // This function adds cards the page to display the data in the array
 function showCards() {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = counter; i < counter+5; i++) {
     let title = Movie[i].Series_Title;   
     
     //image comes small so edit the url for regular dimensions
     let imageURL = Movie[i].Poster_Link;
     imageURL = imageURL.slice(0,imageURL.indexOf("._V1")) + ".jpg";
 
+    let Score = Movie[i].Meta_score;
+
+    let Runtime = Movie[i].Runtime;
+
+    let Overview = Movie[i].Overview;
+
+    let IMDB = Movie[i].Gross;
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard, title, imageURL, Score, Runtime, Overview, Money); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+
+function editCardContent(card, newTitle, newImageURL, Score, Runtime, Overview, Money) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
@@ -36,6 +46,12 @@ function editCardContent(card, newTitle, newImageURL) {
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
+
+  const cardBullet = card.querySelectorAll("li");
+  cardBullet[0].textContent = "Money made: " + Money;
+  cardBullet[1].textContent = "Runtime: " + Runtime;
+  cardBullet[2].textContent = "Synopsis: " + Overview;
+  cardBullet[3].textContent = "Movie_Score: " + Score;
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
@@ -46,14 +62,20 @@ function editCardContent(card, newTitle, newImageURL) {
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
 
-function quoteAlert() {
+function NextButton() {
   console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!",
-  );
+  
+  counter+= 5;
+  showCards(); 
 }
 
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
+function LastButton() {
+  if(counter != 0)
+  counter-=5;
   showCards(); // Call showCards again to refresh
+}
+
+function Bullet()
+{
+  return "something";
 }
