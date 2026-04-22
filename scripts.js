608c10952fc44eb3d1ff86a6254de08435798872
Mike Ze Feng Chen <mchen175@go.pasadena.edu>
2026-04-21 20:22:14 -0700

    final

--- scripts.js
+++ scripts.js
@@ -21,23 +21,25 @@ function showCards() {
     //image comes small so edit the url for regular dimensions
     let imageURL = Movie[i].Poster_Link;
     imageURL = imageURL.slice(0,imageURL.indexOf("._V1")) + ".jpg";
-
+    
     let Score = Movie[i].Meta_score;
 
     let Runtime = Movie[i].Runtime;
 
-    let Overview = Movie[i].Overview;
 
-    let IMDB = Movie[i].Gross;
+    let Money = Movie[i].Gross;
+
+    let Year_date = Movie[i].Released_Year;
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL, Score, Runtime, Overview, Money); // Edit title and image
+    editCardContent(nextCard, title, imageURL, Score, Runtime, Money, Year_date); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 }
 
+let visible = true;
 
-function editCardContent(card, newTitle, newImageURL, Score, Runtime, Overview, Money) {
+function editCardContent(card, newTitle, newImageURL, Score, Runtime, Money, Year) {
   card.style.display = "block";
 
   const cardHeader = card.querySelector("h2");
@@ -47,20 +49,90 @@ function editCardContent(card, newTitle, newImageURL, Score, Runtime, Overview, 
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
 
+  let clicked = false;
+  
+  const cardBullet = card.querySelectorAll("li");
+
+  if(visible == true)
+  {
+    cardBullet[0].textContent = "-Money made: " + Money;
+    cardBullet[1].textContent = "-Runtime: " + Runtime;
+    cardBullet[2].textContent = "-Movie_Score: " + Score;
+    cardBullet[3].textContent = "-Year: " + Year;
+  }
+
+  card.onclick = () => { 
+  if(clicked == true)
+  {
+    clicked = false;
+    cardBullet[0].textContent = "-Money made: " + Money;
+    cardBullet[1].textContent = "-Runtime: " + Runtime;
+    cardBullet[2].textContent = "-Movie_Score: " + Score;
+    cardBullet[3].textContent = "-Year: " + Year;
+  } else
+  {
+    clicked = true;
+    cardBullet[0].textContent = "";
+    cardBullet[1].textContent = "";
+    cardBullet[2].textContent = "";
+    cardBullet[3].textContent = "";
+  }
+  }
+  
+  /*
   const cardBullet = card.querySelectorAll("li");
   cardBullet[0].textContent = "Money made: " + Money;
   cardBullet[1].textContent = "Runtime: " + Runtime;
   cardBullet[2].textContent = "Synopsis: " + Overview;
   cardBullet[3].textContent = "Movie_Score: " + Score;
+  */
 
   // You can use console.log to help you debug!
   // View the output by right clicking on your website,
   // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
 }
+
+
+function Sort(value)
+{
+  console.log(value);
+  console.log(Movie[0][value]);
+  let swap;
+  for(let i = 0; i < Movie.length-1;i++)
+  {
+      if((Movie[i][value]) < (Movie[i+1][value]))
+      {
+        swap = Movie[i];
+        Movie[i] = Movie[i+1];
+        Movie[i+1] = swap;
+        i = -1;
+      }
+  }
+  counter = 0;
+  showCards();
+}
+
 
 // This calls the addCards() function when the page is first loaded
 document.addEventListener("DOMContentLoaded", showCards);
+
+//Buttons and movement
+function HoverRight()
+{
+  NextButton()
+  hoverInterval = setInterval(NextButton, 800);
+}
+
+function HoverLeft()
+{
+  LastButton()
+  hoverInterval = setInterval(LastButton, 800);
+}
+
+function HoverOut()
+{
+  clearInterval(hoverInterval);
+}
 
 function NextButton() {
   console.log("Button Clicked!");
@@ -75,7 +147,3 @@ function LastButton() {
   showCards(); // Call showCards again to refresh
 }
 
-function Bullet()
-{
-  return "something";
-}
